c9b4596c8fe8e2ef7cadce8688b46fc9877ecf30
student <student@ip-172-31-60-137.us-west-2.compute.internal>
2021-06-29 16:53:07 -0700

    finished piglatin

--- src/_10_pig_latin/PigLatin.java
+++ src/_10_pig_latin/PigLatin.java
@@ -1,4 +1,4 @@
-package _09_latest_tweet;
+package _10_pig_latin;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,18 +9,22 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class LatestTweet implements ActionListener {
+public class PigLatin implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JTextField textField = new JTextField(10);
+	JTextField leftTextField = new JTextField(20);
 	JButton button = new JButton();
+	JLabel label = new JLabel();
+	PigLatinTranslator translator = new PigLatinTranslator();
 	
 void run() {
 	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.add(panel);
-	panel.add(textField);
+	panel.add(leftTextField);
 	panel.add(button);
-	button.setText("Search");
+	panel.add(label);
+	button.setText("translate");
 	button.addActionListener(this);
 	frame.pack();
 }
@@ -28,6 +32,9 @@ void run() {
 @Override
 public void actionPerformed(ActionEvent arg0) {
 	// TODO Auto-generated method stub
-	System.out.println("Tweet, Tweet");
+	String output = translator.translate(leftTextField.getText());
+	label.setText(output);
+	frame.pack();
+	}
 }
-}
+
